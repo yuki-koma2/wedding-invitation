8b828287d34f30524a0bd0c0d6134b4cdb969014
yuki-koma2 <yuki.k.lion@gmail.com>
2024-09-30 19:41:03 +0900

    feat: add react hook form and sample code

--- invitation-nextjs/src/app/invitation/page.tsx
+++ invitation-nextjs/src/app/invitation/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Image, Spacer} from "@nextui-org/react";
+import UserFormWithReactHookForm from "@/components/sampleUserFormWithReactHookForm";
 
 
 const HogePage = () => {
@@ -172,7 +173,10 @@ const HogePage = () => {
             <section
                 className="bg-white p-4 font-klee font-bold text-gray-500 text-center border-2 border-gray-200 rounded-lg shadow-sm">
                 <p>お手数ではございますがご都合のほどを11月2日までご一報賜りますようお願い申し上げます。</p>
+                <UserFormWithReactHookForm/>
+
             </section>
+
 
         </main>
     );
