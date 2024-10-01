0220a3dcbfaf2dc28b4a8e88cd2a8a22f6ff2fed
yuki-koma2 <yuki.k.lion@gmail.com>
2024-10-01 22:56:12 +0900

    feat: 親族ページむけのリンクを作成

--- invitation-nextjs/src/config/site.ts
+++ invitation-nextjs/src/config/site.ts
@@ -1,80 +1,85 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
-  navigation:{
-    invitation: {
-      name: "Invitation",
-      description: "Invitation page",
-      path: "/invitation",
-    }
-  },
-  navItems: [
-    {
-      label: "Home",
-      href: "/",
+    name: "Next.js + NextUI",
+    description: "Make beautiful websites regardless of your design experience.",
+    navigation: {
+        invitation: {
+            name: "Invitation",
+            description: "Invitation page",
+            path: "/invitation",
+        },
+        familyInvitation: {
+            name: "Family Invitation",
+            description: "Family invitation page",
+            path: "/family-invitation",
+        }
     },
-    {
-     label: "Invitation",
-      href: "/invitation",
+    navItems: [
+        {
+            label: "Home",
+            href: "/",
+        },
+        {
+            label: "Invitation",
+            href: "/invitation",
+        },
+        {
+            label: "Docs",
+            href: "/docs",
+        },
+        {
+            label: "Pricing",
+            href: "/pricing",
+        },
+        {
+            label: "Blog",
+            href: "/blog",
+        },
+        {
+            label: "About",
+            href: "/about",
+        },
+    ],
+    navMenuItems: [
+        {
+            label: "Profile",
+            href: "/profile",
+        },
+        {
+            label: "Dashboard",
+            href: "/dashboard",
+        },
+        {
+            label: "Projects",
+            href: "/projects",
+        },
+        {
+            label: "Team",
+            href: "/team",
+        },
+        {
+            label: "Calendar",
+            href: "/calendar",
+        },
+        {
+            label: "Settings",
+            href: "/settings",
+        },
+        {
+            label: "Help & Feedback",
+            href: "/help-feedback",
+        },
+        {
+            label: "Logout",
+            href: "/logout",
+        },
+    ],
+    links: {
+        github: "https://github.com/nextui-org/nextui",
+        twitter: "https://twitter.com/getnextui",
+        docs: "https://nextui.org",
+        discord: "https://discord.gg/9b6yyZKmH4",
+        sponsor: "https://patreon.com/jrgarciadev",
     },
-    {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
-  ],
-  navMenuItems: [
-    {
-      label: "Profile",
-      href: "/profile",
-    },
-    {
-      label: "Dashboard",
-      href: "/dashboard",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
-    {
-      label: "Logout",
-      href: "/logout",
-    },
-  ],
-  links: {
-    github: "https://github.com/nextui-org/nextui",
-    twitter: "https://twitter.com/getnextui",
-    docs: "https://nextui.org",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
-  },
 };
