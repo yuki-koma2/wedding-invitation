20b21424f7e9b7ac63ccc956757b341c10e000cb
yuki-koma2 <yuki.k.lion@gmail.com>
2024-10-05 21:23:17 +0900

    feat: 細かい文言調整

--- invitation-nextjs/src/app/page.tsx
+++ invitation-nextjs/src/app/page.tsx
@@ -6,81 +6,89 @@ import {button as buttonStyles} from "@nextui-org/theme";
 import {siteConfig} from "@/config/site";
 import {title, subtitle} from "@/components/primitives";
 import {GithubIcon} from "@/components/icons";
+import MessageSection from "@/components/Message";
+import InformationSection from "@/components/Information";
+import GallerySection from "@/components/Gallery";
+import GuestForm from "@/components/GuestForm";
+import React from "react";
 
 export default function Home() {
     return (
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-            <div className="inline-block max-w-xl text-center justify-center">
-                <h1 className={title()}>Make&nbsp;</h1>
-                <h1 className={title({color: "violet"})}>beautiful&nbsp;</h1>
-                <br/>
-                <h1 className={title()}>
-                    websites regardless of your design experience.
-                </h1>
-                <h2 className={subtitle({class: "mt-4"})}>
-                    Beautiful, fast and modern React UI library.
-                </h2>
-            </div>
+        <main
+            className={`bg-[#fffcf9]`}
+        >
 
-            <div className="flex gap-3">
-                <Link
-                    className={buttonStyles({
-                        color: "primary",
-                        radius: "full",
-                        variant: "shadow",
-                    }) + "bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"}
-                    href={siteConfig.navigation.invitation.path}
-                >
-                    invitation
-                </Link>
-                <Link
-                    className={buttonStyles({
-                        color: "primary",
-                        radius: "full",
-                        variant: "shadow",
-                    }) + "bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"}
-                    href={siteConfig.navigation.familyInvitation.path}
-                >
-                    親族向けページ
-                </Link>
-                <Link
-                    className={buttonStyles({
-                        color: "primary",
-                        radius: "full",
-                        variant: "shadow",
-                    }) + "bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"}
-                    href={siteConfig.navigation.thanks.path}
-                >
-                    tanksページ
-                </Link>
-                {/*<Link*/}
-                {/*    isExternal*/}
-                {/*    className={buttonStyles({*/}
-                {/*        color: "primary",*/}
-                {/*        radius: "full",*/}
-                {/*        variant: "shadow",*/}
-                {/*    })}*/}
-                {/*    href={siteConfig.links.docs}*/}
-                {/*>*/}
-                {/*    Documentation*/}
-                {/*</Link>*/}
-                {/*<Link*/}
-                {/*    isExternal*/}
-                {/*    className={buttonStyles({variant: "bordered", radius: "full"})}*/}
-                {/*    href={siteConfig.links.github}*/}
-                {/*>*/}
-                {/*    <GithubIcon size={20}/>*/}
-                {/*    GitHub*/}
-                {/*</Link>*/}
-            </div>
+            <section
+                className="
+                h-screen min-h-96 relative flex flex-1 shrink-0 items-center justify-center overflow-hidden rounded-lg bg-gray-100 py-16 shadow-lg md:py-20 xl:py-48
+                ">
+                <img src={`/img/header.jpeg`}
+                     loading="lazy"
+                     alt="結婚指輪と花束"
+                     className="absolute inset-0 h-full w-full object-cover object-center"/>
 
-            <div className="mt-8">
-                <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-                </Snippet>
-            </div>
-        </section>
+                <div className="absolute inset-0 bg-gray-300 mix-blend-multiply"/>
+
+                <div className="relative flex flex-col items-center p-4 sm:max-w-xl">
+
+                    <h1 className="font-petit text-6xl text-white">
+                        Wedding<br/> Invitation
+                    </h1>
+                    <br/>
+
+                    <p className="font-shadows text-3xl text-white text-center pt-3">
+                        2024/12/1 <br/>
+                        yuki & natsumi
+                    </p>
+
+
+                </div>
+            </section>
+            <MessageSection/>
+            <InformationSection />
+            <GallerySection/>
+            <section
+                className="p-4 font-klee font-bold text-gray-500 text-center rounded-lg ">
+                <p className="my-10">お手数ではございますがご都合のほどを<br/><span
+                    className="font-bold">11月2日(土)</span>までにご一報賜りますようお願い申し上げます</p>
+                <GuestForm/>
+            </section>
+            <hr />
+            <footer className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 bg-pink-50">
+                <div className="flex gap-3">
+                    <Link
+                        className={buttonStyles({
+                            color: "primary",
+                            radius: "full",
+                            variant: "shadow",
+                        }) + "bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"}
+                        href={siteConfig.navigation.invitation.path}
+                    >
+                        友人向け
+                    </Link>
+                    <Link
+                        className={buttonStyles({
+                            color: "primary",
+                            radius: "full",
+                            variant: "shadow",
+                        }) + "bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"}
+                        href={siteConfig.navigation.familyInvitation.path}
+                    >
+                        親族向け
+                    </Link>
+                    <Link
+                        className={buttonStyles({
+                            color: "primary",
+                            radius: "full",
+                            variant: "shadow",
+                        }) + "bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg"}
+                        href={siteConfig.navigation.thanks.path}
+                    >
+                        thanks
+                    </Link>
+
+                </div>
+            </footer>
+        </main>
     );
 }
