5f12e24e8f69d79fa1f816ba02ef6e1550627b58
yuki-koma2 <yuki.k.lion@gmail.com>
2024-10-05 15:11:14 +0900

    chore: ロギングの出力を調整

--- invitation-nextjs/src/lib/prisma.ts
+++ invitation-nextjs/src/lib/prisma.ts
@@ -14,7 +14,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
     log: process.env.NODE_ENV === 'production'
-        ? ['error']
+        ? ['error', 'warn']
         : ['query', 'info', 'warn', 'error'],
 })
 
