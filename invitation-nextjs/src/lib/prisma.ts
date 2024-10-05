5b70a06aafb987d388240b0f0ce72179709eb960
yuki-koma2 <yuki.k.lion@gmail.com>
2024-10-05 14:27:32 +0900

    feat: guest情報をpostできるようにした

--- invitation-nextjs/src/lib/prisma.ts
+++ invitation-nextjs/src/lib/prisma.ts
@@ -10,9 +10,17 @@ import { PrismaClient } from '@prisma/client'
 // Learn more:
 // https://pris.ly/d/help/next-js-best-practices
 
+
+// declare global {
+//     // グローバルオブジェクトに PrismaClient のインスタンスを保存
+//     var prisma: PrismaClient | undefined;
+// }
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'], // 必要に応じてログレベルを調整
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
---
+++ invitation-nextjs/src/pages/api/guest.ts
@@ -0,0 +1,52 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from "@/lib/prisma";
+import { GuestSchema } from "@/components/GuestForm/guest.schema";
+import { ZodError } from "zod";
+
+// POST /api/guest
+export default async function handle(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    if (req.method !== 'POST') {
+        res.setHeader('Allow', ['POST']);
+        return res.status(405).json({message: `Method ${req.method} Not Allowed`});
+    }
+
+    try {
+        // TODO リクエストボディのパースとバリデーション フロントと同じなので変えたい。
+        const data = GuestSchema.parse(req.body);
+
+        // データベースへの挿入
+        const newGuest = await prisma.guest.create({
+            data: {
+                firstName: data.firstName,
+                lastName: data.lastName,
+                firstNameKana: data.firstNameKana,
+                lastNameKana: data.lastNameKana,
+                side: data.side,
+                postalCode: data.postalCode || null,
+                prefectureCode: data.prefectureCode,
+                city: data.city || null,
+                addressLine: data.addressLine || null,
+                attendanceStatus: data.attendanceStatus,
+            },
+        });
+
+        return res.status(201).json({message: 'データを保存しました', guest: newGuest});
+    } catch (error) {
+        if (error instanceof ZodError) {
+            return res.status(400).json({
+                message: '入力データに誤りがあります',
+                errors: error.errors.map((e) => ({
+                    path: e.path.join('.'),
+                    message: e.message,
+                })),
+            });
+        }
+        // TODO error handling
+        console.error('サーバーエラー:', error);
+        return res.status(500).json({message: 'サーバーエラーが発生しました'});
+        // return res.status(201).json(result);
+    }
+}
